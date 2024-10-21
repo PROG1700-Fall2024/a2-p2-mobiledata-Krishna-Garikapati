c5e228efb37967a3fee6c11a06bb7e99da3a0d95
krishna-garikapati <w0502117@campus.nscc.ca>
2024-10-21 11:59:57 -0300

    Ready for marking

--- A2-P2-MobileData.py
+++ A2-P2-MobileData.py
@@ -5,19 +5,21 @@
 # Note, it is not a cumulative charge; your program will figure out which single range the usage falls into, 
 # then calculate the cost based on that range’s cost. 
 
-#Student #:     
-#Student Name:  
+#Student #: Krishna Priyanka Garikapati    
+#Student Name:  W0502117
 
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
-
-
-
-
-
-
-
-    # YOUR CODE ENDS HERE
-
+    #input
+        DataUsage=int(input("Enter data usage (Mb): "))
+    #Conditions
+        if DataUsage<=200:
+            TotalCharge=20
+        elif 500>=DataUsage>200:
+            TotalCharge=20+((DataUsage-200)*0.105)+1
+        elif 1000>=DataUsage>500:
+            TotalCharge=20+(300*(0.105))+((DataUsage-500)*0.105)
+        elif DataUsage>1000:
+            TotalCharge=118.00
+    #Output
+        print("Total charge is ${0:.2f}".format(TotalCharge) )
 main()
